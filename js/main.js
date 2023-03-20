308a96f55d5ace3a917ca22c3edaddf4601d25c6
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-20 09:21:04 -0400

    Added dim background upon opening mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let open_menu_btn = document.querySelector("#icon-menu");
 let close_menu_btn = document.querySelector("#close-icon");
+let body = document.querySelector("body");
 
 let popup = document.querySelector(".menu-popup");
 let background = document.querySelector(".background-darken");
@@ -7,11 +8,11 @@ let background = document.querySelector(".background-darken");
 // Open menu on button click
 open_menu_btn.addEventListener("click", function() {
     popup.style.display = "block";
-    background.style.opacity = "20%";
+    body.classList.add("background-dim");
 })
 
 // Close menu on button click
 close_menu_btn.addEventListener("click", function() {
     popup.style.display = "none";
-    background.style.display = "0%";
+    body.classList.remove("background-dim");
 })
